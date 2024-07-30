908fb8239c6bb06c186469d6f316e10e4cfb8580
Matthew Ganaden <mjdganaden@gmail.com>
2024-07-30 23:20:48 +0800

    changes to projects

--- script/Projects.js
+++ script/Projects.js
@@ -1,3 +1,14 @@
 function displayDesign() {
+    document.getElementById("title").innerHTML = 'Design';
+    document.getElementById("description").innerHTML = 'description for design';
+}
 
+function displayWebDev() {
+    document.getElementById("title").innerHTML = 'Web Development';
+    document.getElementById("description").innerHTML = 'description for Web Development';
+}
+
+function displayTesting() {
+    document.getElementById("title").innerHTML = 'Testing and Arduino';
+    document.getElementById("description").innerHTML = 'I have used my knowledge in selenium to create a custom facebook scraper for our thesis. I have also';
 }
